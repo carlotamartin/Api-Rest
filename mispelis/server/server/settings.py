30184d51485c204d3425fea8ff30ee5ad311592c
carlotamartin <91574355+carlotamartin@users.noreply.github.com>
2022-12-03 18:32:51 +0100

    codificando contraseñas

--- mispelis/server/server/settings.py
+++ mispelis/server/server/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'rest_framework',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -40,6 +41,7 @@ INSTALLED_APPS = [
 
     #Django external apps
     'rest_framework',
+    'django_rest_passwordreset',
 
     # Django custom apps
     'authentication',
