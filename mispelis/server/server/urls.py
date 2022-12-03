6f1dbf7dce5fa796211911864ba4aee5afd9ba49
carlotamartin <91574355+carlotamartin@users.noreply.github.com>
2022-12-03 18:32:59 +0100

    codificando contraseñas

--- mispelis/server/server/urls.py
+++ mispelis/server/server/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from django.urls import path
+from rest_framework import routers
+#Api router
+router = routers.DefaultRouter()
 
 urlpatterns = [
     path('admin/', admin.site.urls),
