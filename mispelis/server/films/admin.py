158836d74b07ded2f398bea995c52de6f363c5d0
carlotamartin <91574355+carlotamartin@users.noreply.github.com>
2022-12-07 13:18:30 +0100

    films

--- mispelis/server/films/admin.py
+++ mispelis/server/films/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib import admin
+from .models import Film, FilmGenre
+
+
+@admin.register(Film)
+class FilmAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(FilmGenre)
+class FilmGenreAdmin(admin.ModelAdmin):
+    readonly_fields = ["slug"]
--- mispelis/server/server/urls.py
+++ mispelis/server/server/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework import routers
+from django.conf import settings
+from django.conf.urls.static import static
+
 #Api router
 router = routers.DefaultRouter()
 
