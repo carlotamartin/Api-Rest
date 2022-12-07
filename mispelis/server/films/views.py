606e107334cd41f4c42ff9003a53728c5460a348
carlotamartin <91574355+carlotamartin@users.noreply.github.com>
2022-12-07 17:46:12 +0100

    cambios

--- mispelis/server/films/views.py
+++ mispelis/server/films/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
-from rest_framework import viewsets, filters
-from .models import Film, FilmGenre
-from .serializers import FilmSerializer, FilmGenreSerializer
+from rest_framework import viewsets, filters, status, views
+from .models import Film, FilmGenre, FilmUser
+from .serializers import FilmSerializer, FilmGenreSerializer, FilmUserSerializer
 from django_filters.rest_framework import DjangoFilterBackend   # new
 from rest_framework.pagination import PageNumberPagination  # new
 from rest_framework.response import Response  # new
@@ -63,5 +63,46 @@ class FilmViewSet(viewsets.ReadOnlyModelViewSet):
     # Sistema de paginación
     pagination_class = ExtendedPagination
     pagination_class.page_size = 8  # películas por página
+
+
+class FilmUserViewSet(views.APIView):
+    
+    # El método GET devolverá las peliculas del usuario
+    def get(self, request, *args, **kwargs):
+        queryset = FilmUser.objects.filter(user=self.request.user)
+        serializer = FilmUserSerializer(queryset, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    # El método POST permitirá gestionar su información de la película
+    def post(self, request, *args, **kwargs):
+        try:
+            film = Film.objects.get(id=request.data['uuid'])
+        except Film.DoesNotExist:
+            return Response(
+                {'status': 'Film not found'},
+                status=status.HTTP_404_NOT_FOUND)
+
+        # Una vez recuperada la película creamos o recuperamos su FilmUser
+        film_user, created = FilmUser.objects.get_or_create(
+            user=request.user, film=film)
+
+        # Configuramos cada campo
+        film_user.state = request.data.get('state', 0)
+        film_user.favorite = request.data.get('favorite', False)
+        film_user.note = request.data.get('note', -1)
+        film_user.review = request.data.get('review', None)
+
+        # Si se marca la pelicula como NO VISTA la borramos automáticamente
+        if int(film_user.state) == 0:
+            film_user.delete()
+            return Response(
+                {'status': 'Deleted'}, status=status.HTTP_200_OK)
+
+        # En otro caso guardamos los campos de la película de usuario
+        else:
+            film_user.save()
+
+        return Response(
+            {'status': 'Saved'}, status=status.HTTP_200_OK)
 
 
